a4656cca7335bbfba09c6dbb51d927227a4dc7d1
voidnire <nirerin15@gmail.com>
2022-05-02 07:26:42 -0400

    Adicionar arquivos de projeto.

---
+++ TesteSalvaremDocs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TesteSalvaremDocs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sr = new StreamWriter(@"C:\Users\USERNAME\Desktop\FOLDERNAME\FileSmth.txt", true);
+            Console.WriteLine("Write something down: ");
+            string smth = Console.ReadLine();
+            sr.WriteLine(smth);
+            sr.Close();
+            Console.ReadLine();
+        }
+    }
+}
